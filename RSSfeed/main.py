ac816c94b8d2721968e4454314d61ffec9be5815
joao <jd3870894@gmail.com>
2026-03-13 23:41:56 -0300

    feat: add the initial draft of the function that fetches the sources

--- RSSfeed/main.py
+++ RSSfeed/main.py
@@ -1,7 +1,31 @@
+# Here is the function that fetches the XML from the RSS source.
+# It runs every 4 hours. This interval is arbitrary and can be changed.
+# Keep in mind that if you add many RSS sources, processing all links may lead to performance issues.
 import feedparser
-url = "https://feeds.folha.uol.com.br/emcimadahora/rss091.xml"
-f = feedparser.parse(url)
-# print(f)
-print(f.entries[0].title)
+import time
+from datetime import datetime
+from feeds.brasil import RSS_FEED
+
+# this while true is ugly and will be changed.
+
+while True:
+    for feed in RSS_FEED:
+        print(f"Fetching feed: {feed}" )
+        f = feedparser.parse(feed) 
+
+        for entry in f.entries:
+            title = entry.get("title")
+            url = entry.get("link")
+            published_parsed = entry.get("published_parsed")
+                
+            print(title)
+
+            if not url:
+                continue
+
+
+
+    time.sleep(10)
+
 
  
